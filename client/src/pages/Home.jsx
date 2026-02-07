f038c87020e90a9c72206e2b674bee01ba59ff83
sarim-alis <sarimslayerali786@gmail.com>
2026-02-07 18:55:35 +0500

    feat: interview changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,9 +10,7 @@ const Home = () => {
       <GenInfo />
       <Brands />
       <div className="md:w-full md:max-w-full xs:mx-2  sm:mx-auto ">
-        <div className="prose prose-2xl">
-          <ShopBy title="Best Sellers" filter="bestSellers" />
-        </div>
+        <Brands title="Best Sellers" />
         <div className="prose prose-2xl">
           <ShopBy title="Top Rated" filter="topRated" />
         </div>
